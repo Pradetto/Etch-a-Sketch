a14971977bf043779b141c61deaa47f1bbf807ae
Michael Pradetto <pradetto5@gmail.com>
2022-06-18 17:54:12 -0700

    finalized project

--- scripts.js
+++ scripts.js
@@ -5,38 +5,79 @@
 // test.textContent = 'Helllooo'
 
 
+
+
 // creating grid
-const container = document.querySelector('#container')
-let size = 16 //prompt('What container size do you want 16-64?')
 
-function grid(){
+// getting slider value
 
+const sliderValue = document.querySelector('#slider');
+const para = document.querySelector('span');
+para.textContent = '16x16';
+sliderValue.addEventListener('input', () => {
+    para.textContent = `${sliderValue.value}x${sliderValue.value}`;
+    grid(sliderValue.value);
+});
+
+const container = document.querySelector('#container');
+// let size = sliderValue.value //prompt('What container size do you want 16-64?')
+let gridSize = 600;
+// console.log(gridSize/size);
+// console.log(sliderValue.value);
+
+
+function grid(size){
+    container.innerHTML='';
     for (let i = 0; i < size*size;i++) {
         let square = document.createElement('div');
         square.classList.add('box');
         // square.textContent = `${i}`;
+        square.style.width = (gridSize/(size * 1) - 2).toString() + 'px';
+        square.style.height = (gridSize/(size * 1) - 2).toString() + 'px';
         container.appendChild(square);
         square.addEventListener('mouseover', function (e) {
             e.target.style.background = 'black';
         });
     };
 };
-grid()
+grid(16)
 
-// function rgb(){
 
-// }
+let size = 50;
 
 // Button Controls Below:
 const children = container.childNodes;
+
+// Random Color:
+function random(){
+    randomColor='#'+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor
+} 
+
+const rgb = document.querySelector('#rgb');
+rgb.addEventListener('click', () => {
+    for (let i=0;i<size*size;i++){
+        if (children[i] === undefined){
+            continue;
+        } else {
+        children[i].addEventListener('mouseover', function(e){
+            e.target.style.background = random();
+            });
+        };
+    };
+});
 
 //Black Button:
 const black = document.querySelector('#black');
 black.addEventListener('click', () => {
     for (let i = 0; i < size*size;i++){
+                if (children[i] === undefined){
+            continue;
+        } else {
         children[i].addEventListener('mouseover', function(e){
-            e.target.style.background = 'red';
-        });
+            e.target.style.background = 'black';
+            });
+        };
     };
 });
 
@@ -51,14 +92,7 @@ reset.addEventListener('click', () => {
 
 
 
-// getting slider value
 
-const sliderValue = document.querySelector('#slider');
-const para = document.querySelector('p')
-para.textContent = '16'
-sliderValue.addEventListener('input', () => {
-    para.textContent = `${sliderValue.value}`
-});
 
 
 
